09ac7df0527a9e68c56d175476f7676c85269197
codekarimi <ckarimi676@gmail.com>
2025-07-04 10:21:18 +0300

    modified app layout

--- prepwise/app/layout.tsx
+++ prepwise/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${monaSans.className} antialiased pattern`}
+        className={`${monaSans.className} antialiased `}
       >
         {children}
       </body>
