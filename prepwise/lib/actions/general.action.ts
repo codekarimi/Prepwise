cebd91d78700c8f396f7580c57d22f1babc09e99
codekarimi <ckarimi676@gmail.com>
2025-07-17 10:59:57 +0300

    First Phase deployment

--- prepwise/lib/actions/general.action.ts
+++ prepwise/lib/actions/general.action.ts
@@ -97,7 +97,7 @@ export async function getLatestInterviews(
 
   const interviews = await db
     .collection("interviews")
-    .orderBy("createdAt", "desc")
+    // .orderBy("createdAt", "desc")
     .where("finalized", "==", true)
     .where("userId", "!=", userId)
     .limit(limit)
@@ -115,7 +115,7 @@ export async function getInterviewsByUserId(
   const interviews = await db
     .collection("interviews")
     .where("userId", "==", userId)
-    .orderBy("createdAt", "desc")
+    // .orderBy("createdAt", "desc")
     .get();
 
   return interviews.docs.map((doc) => ({
